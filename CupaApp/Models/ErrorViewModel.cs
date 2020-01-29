746873de4fdf0d9c9812153435e46ee39426adac
Thomas Gräupner <Thomas.Graeupner@plan-b-gmbh.com>
2020-01-29 09:51:01 +0100

    123

--- CupaApp/Models/ErrorViewModel.cs
+++ CupaApp/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace CupaApp.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string HalloId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
